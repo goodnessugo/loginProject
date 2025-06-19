3e83cd217e7052f296192b1e48977373a801c642
goodnessugo <emusical123@gmail.com>
2025-06-19 09:48:52 +0100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='bg-gray-100 h-[100vh] flex justify-center items-center'>
+
+
+            <form action="" className='flex flex-col bg-gray-100 h-fit p-10 styleshadow rounded-3xl '>
+                <input type="text" placeholder='email' name='Email' className='w-[70vw] styleshadow rounded-lg p-3 my-2 outline-none text-gray-500' />
+                <input type="password" placeholder='Password' />
+            </form>
         </div>
     )
 }
