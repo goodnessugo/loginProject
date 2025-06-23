c22d4ecd8de5425f5492fe8386081bc61275814d
goodnessugo <emusical123@gmail.com>
2025-06-23 17:35:00 +0100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import { MdEmail } from "react-icons/md";
 import { GiPadlockOpen } from "react-icons/gi";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { BsShieldLock } from "react-icons/bs";
+import  GOOGLE from '../images/google.webp'
+import  FACEBOOK from '../images/Facebook_Logo_2023.png'
+import  APPLE from '../images/apple.png'
 
 const Login = () => {
     return (
-        <div className='bg-gray-100 h-[100vh] flex flex-col justify-center items-center'>
+        <div className='bg-gray-100 h-[100vh] flex items-center flex-col text-gray-500 p-5 '>
 
-            <div className='h-20 w-20 flex justify-center items-center text-9xl text-gray-300 rounded-full bg-gray-100 styleshadowinset z-20 mb-5 ' >
-                <IoPersonCircleOutline />
+            <div className='h-15 w-15 flex justify-center items-center text-5xl text-gray-300 rounded-full bg-gray-100 styleshadow z-20 mb-2' >
+                <BsShieldLock />
             </div>
+
+
+            <div className='pb-10 flex flex-col items-center text-gray-400 '>
+                <h1 className='text-2xl font-bold'>Login Account</h1>
+                <p>Welcome back to app</p>
+            </div>
+
             <form action="" className='flex flex-col bg-gray-100 h-fit w-[80vw] md:w-[30vw] p-5 styleshadow rounded-3xl '>
                 <div className=' flex items-center gap-1 w-full styleshadow rounded-lg p-3 my-2 text-gray-500'>
                     <MdEmail /> <input type="text" placeholder='email' name='Email' className='outline-none text-gray-500 ' />
@@ -19,7 +30,34 @@ const Login = () => {
                     <GiPadlockOpen /> <input type="password" placeholder='Password' className='outline-none text-gray-500 ' />
 
                 </div>
+
+                <div className='flex justify-end text-sm pb-5 '>
+                    <h1 >Forgot password?</h1>
+                </div>
+
+
+                <button className=' bg-gray-300 hover:bg-gray-600 hover:text-gray-200 ease-in-out cursor-pointer p-4 rounded-md font-bold text-gray-800'>Login</button>
+
+                <div className='flex flex-col justify-center items-center pt-5'>
+                    <h1 className='text-xs'>Or sign up with</h1>
+                    
+                    {/* social icons */}
+                    <div className="socialMedia flex justify-center items-center gap-5 cursor-pointer">
+                        <img src={GOOGLE} alt="" className='h-17'/>
+                        <img src={FACEBOOK} alt="" className='h-12'/>
+                        <img src={APPLE} alt="" className='h-13'/>
+                    </div>
+                </div>
+
+
             </form>
+
+
+
+            <div>
+
+            </div>
+
         </div>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { GiPadlockOpen } from 'react-icons/gi'
+import { IoPersonCircleOutline } from 'react-icons/io5'
+import { MdEmail } from 'react-icons/md'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className='bg-gray-100 h-[100vh] flex flex-col justify-center items-center'>
+
+            <div className='h-20 w-20 flex justify-center items-center text-9xl text-gray-300 rounded-full bg-gray-100 styleshadowinset z-20 mb-5 ' >
+                <IoPersonCircleOutline />
+            </div>
+            <form action="" className='flex flex-col bg-gray-100 h-fit w-[80vw] md:w-[30vw] p-5 styleshadow rounded-3xl '>
+                {/* fullname section */}
+                <div className=' flex items-center gap-1 w-full styleshadow rounded-lg p-3 my-2 text-gray-500'>
+                    <IoPersonCircleOutline /> <input type="text" placeholder='fullname' name='fullname' className='outline-none text-gray-500 ' />
+                </div>
+
+                {/* email section*/}
+                <div className=' flex items-center gap-1 w-full styleshadow rounded-lg p-3 my-2 text-gray-500'>
+                    <MdEmail /> <input type="text" placeholder='email' name='Email' className='outline-none text-gray-500 ' />
+                </div>
+
+                {/* password section */}
+                <div className=' flex items-center gap-1 w-full styleshadowinset rounded-lg p-3 my-2 text-gray-500'>
+                    <GiPadlockOpen /> <input type="password" placeholder='Password' className='outline-none text-gray-500 ' />
+
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Register
